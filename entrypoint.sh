d4586d58bb75c5842522de6fdad472ef7eae321b
June1991 <18681585220@163.com>
2020-07-31 16:10:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 # Using this script because github action passess the arguments as strings
 # instead of array
 echo ">>> Running sls deploy $@"
-sls deploy --debug
+sls deploy --debug $@
 echo ">>> Compleated"
